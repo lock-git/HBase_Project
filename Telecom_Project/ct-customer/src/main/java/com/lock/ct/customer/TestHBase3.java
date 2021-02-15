68567938ad76200fd0aac144d8ef078dd53fa2a9
lock <nihao@163.com>
2021-02-15 22:52:18 +0800

    被叫放到被叫位置，flag区分主叫和被叫 [主叫] 0_13349743393_20210101_17318972367_3521

--- Telecom_Project/ct-customer/src/main/java/com/lock/ct/customer/TestHBase3.java
+++ Telecom_Project/ct-customer/src/main/java/com/lock/ct/customer/TestHBase3.java
@@ -36,34 +36,33 @@ public class TestHBase3 {
 
             // 分区号是通过月份计算的，反过来通过月份得到分区号
             List<String[]> startStopRows = HBaseUtil.getStartStopRows(call, start, end);
+            for (String[] startStopRow : startStopRows) {
+                // 根据查询条件查询数据,按照月份分开查询
+                Scan scan = new Scan();
+                // 包含开始
+                // rowKey ==> regionNum + call1 + calldution + call2 + duration
+                String startRow = startStopRow[0];
+                String endRow = startStopRow[1];
 
+                scan.setStartRow(Bytes.toBytes(startRow));
 
+                // 不包含结束
+                scan.setStartRow(Bytes.toBytes(endRow));
 
-            // 根据查询条件查询数据
-            Scan scan = new Scan();
-            // 包含开始
+                // 希望可以包含结束值
+//                InclusiveStopFilter stopFilter = new InclusiveStopFilter(Bytes.toBytes("5_19683537136"));
+//                scan.setFilter(stopFilter);
 
-            // rowKey ==> regionNum + call1 + calldution + call2 + duration
-            String startRow = "4_" + call + "_" + start;
-            String endRow = "1_" + call + "_" + end;
+                ResultScanner resultScanner = table.getScanner(scan);
 
-            scan.setStartRow(Bytes.toBytes(startRow));
-
-            // 不包含结束
-            scan.setStartRow(Bytes.toBytes(endRow));
-
-            // 希望可以包含结束值
-            InclusiveStopFilter stopFilter = new InclusiveStopFilter(Bytes.toBytes("5_19683537136"));
-            scan.setFilter(stopFilter);
-
-            ResultScanner resultScanner = table.getScanner(scan);
-
-            // 遍历得到数据
-            for (Result result : resultScanner) {
-                for (Cell cell : result.rawCells()) {
-                    System.out.println(Bytes.toString(CellUtil.cloneRow(cell)));
+                // 遍历得到数据
+                for (Result result : resultScanner) {
+                    for (Cell cell : result.rawCells()) {
+                        System.out.println(Bytes.toString(CellUtil.cloneRow(cell)));
+                    }
                 }
             }
+
             HBaseUtil.end();
 
 
